aa1fffe6af540c1b1242947708cb75828686ef26
Gerald Baalham <geraldbaalham@live.co.uk>
2025-09-26 11:22:10 +0100

    feat: implement AI model selection with context management and Firebase integration

--- app/_components/AiMultiModels.jsx
+++ app/_components/AiMultiModels.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react"
+'use client'
+
+import { useContext, useState } from "react"
 
 import {
   Select,
   SelectContent,
+  SelectGroup,
   SelectItem,
+  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
@@ -11,44 +15,77 @@ import {
 import AiModelList from './../../shared/AiModelList'
 import Image from "next/image"
 import { Switch } from "@/components/ui/switch"
-import { LockIcon, MessageSquare } from "lucide-react"
+import { Lock, LockIcon, MessageSquare } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { AiSelectedModel } from "@/context/AiSelectedModelContext"
+import { doc, updateDoc } from "firebase/firestore"
+import { db } from "@/config/FirebaseConfig"
+import { useUser } from "@clerk/nextjs"
 function AiMultiModels() {
+
+const { AiSelectedModels, setAiSelectedModels } = useContext(AiSelectedModel)
+const {user} = useUser()
   const [aiModelList, setAiModelList] = useState(AiModelList)
 
-  const onToggleChange =(model, value) => {
+  const onToggleChange = (model, value) => {
     setAiModelList((prev) =>
-    prev.map((m)=>
-    m.model ===model?{...m, enable: value}:m))
+      prev.map((m) =>
+        m.model === model ? { ...m, enable: value } : m))
   }
+
+const onSelectValue = async (parentModel, value)=> {
+  setAiSelectedModels((prev) => ({
+    ...prev,
+    [parentModel]: {
+      
+      modelId: value
+    }
+  }))
+
+  const docRef=doc(db, 'users',user?.primaryEmailAddress?.emailAddress)
+  await updateDoc(docRef, {
+    selectedModelPref: AiSelectedModels
+  })
+}
+
   return (
     <div className="flex flex-1 h-[75vh] border-b">
       {aiModelList.map((model, index) => (
         <div key={index} className={`flex flex-col border-r h-full overflow-auto ${model.enable ? 'flex-1 min-w-[400px]' : 'min-w-[100px]'}`}>
-        <div className="flex w-full items-center justify-between border-b p-4 h-[70px]" key={index}>
-          <div className="flex items-center gap-4">
-            <Image src={model.icon} alt={model.model} width={24} height={24} className="" />
-            {model.enable && <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder={model.subModel[0].name} />
-              </SelectTrigger>
-              <SelectContent>
-                {model.subModel.map((subModel, index) => (<SelectItem value={subModel.name} key={index}>{subModel.name}</SelectItem>))}
-              </SelectContent>
-            </Select>}
+          <div className="flex w-full items-center justify-between border-b p-4 h-[70px]" key={index}>
+            <div className="flex items-center gap-4">
+              <Image src={model.icon} alt={model.model} width={24} height={24} className="" />
+              {model.enable && <Select defaultValue={AiSelectedModels[model.model].modelId} onValueChange={(value) => onSelectValue(model.model, value)} disabled={model.premium} >
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder={AiSelectedModels[model.model].modelId} />
+                </SelectTrigger>
+                <SelectContent>
+
+                  <SelectGroup className='px-3'>
+                    <SelectLabel className='text-sm text-gray-400'>Free</SelectLabel>
+                    {model.subModel.map((subModel, index) => subModel.premium == false && (<SelectItem value={subModel.id} key={index}>{subModel.name}</SelectItem>))}
+                    </SelectGroup>
+
+                    <SelectGroup className='px-3'>
+                    <SelectLabel className='text-sm text-gray-400'>Premium</SelectLabel>
+                    {model.subModel.map((subModel, index) => subModel.premium == true && (<SelectItem disabled={subModel.premium} value={subModel.name} key={index}>{subModel.name} {subModel.premium&&<Lock className="h-4 w-4"/>}</SelectItem>))}
+                    </SelectGroup >
+
+                </SelectContent>
+              </Select>}
+            </div>
+            <div>
+              {model.enable ? <Switch checked={model.enable}
+                onCheckedChange={(v) => onToggleChange(model.model, v)} /> : <MessageSquare onClick={() => onToggleChange(model.model, true)} />}
+            </div>
           </div>
-          <div>
-           {model.enable ?  <Switch checked={model.enable} 
-            onCheckedChange={(v)=> onToggleChange(model.model, v)}/>: <MessageSquare onClick={() => onToggleChange(model.model, true)}/>}
-          </div>
-        </div>
-        {model.premium && model.enable &&<div className="flex items-center justify-center h-full">
-        <Button><LockIcon/>Upgrade to unlock</Button>
-      </div>}
+          {model.premium && model.enable && <div className="flex items-center justify-center h-full">
+            <Button><LockIcon />Upgrade to unlock</Button>
+          </div>}
         </div>
       ))}
-      
-      
+
+
 
     </div>
   )
--- app/provider.jsx
+++ app/provider.jsx
@@ -7,12 +7,17 @@ import AppHeader from "./_components/AppHeader"
 import { useUser } from "@clerk/nextjs"
 import { db } from "@/config/FirebaseConfig"
 import { doc, getDoc, setDoc } from "firebase/firestore"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { AiSelectedModel } from "@/context/AiSelectedModelContext"
+import { DefaultModel } from "@/shared/AiModelsShared"
+import { UserDetailContext } from "@/context/UserDetailContext"
 
 function Provider({children,
     ...props}) {
 
         const {user} = useUser()
+const [AiSelectedModels, setAiSelectedModels] = useState(DefaultModel)
+const [userDetail, setUserDetail] = useState()
 
 useEffect(() => {
     if(user) {
@@ -20,12 +25,17 @@ useEffect(() => {
     }
 }, [user])
 
+
+
 const CreateNewUser = async () => {
     const userRef =doc(db, 'users', user?.primaryEmailAddress?.emailAddress)
     const userSnap = await getDoc(userRef)
 
     if (userSnap.exists()) {
         console.log('user already exists in DB')
+        const userInfo = userSnap.data()
+        setAiSelectedModels(userInfo.selectedModelPref)
+        setUserDetail(userInfo)
         return
 } else {
     const userData={
@@ -37,6 +47,7 @@ const CreateNewUser = async () => {
         credits: 1000
     }
     await setDoc(userRef, userData)
+    setUserDetail(userData)
     console.log('new user created in DB')
 }
 
@@ -49,13 +60,14 @@ const CreateNewUser = async () => {
             enableSystem
             disableTransitionOnChange
             {...props}>
-
+                <UserDetailContext.Provider value={{userDetail, setUserDetail}}>
+            <AiSelectedModel.Provider value={{AiSelectedModels, setAiSelectedModels}}>
                 <SidebarProvider>
                     <AppSidebar/>
                    
                     
             <div className="w-full"><AppHeader />{children}</div></SidebarProvider>
-
+</AiSelectedModel.Provider></UserDetailContext.Provider>
         </NextThemesProvider>
     )
 }
